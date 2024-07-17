278e03ac5bd331839b467490bbbc0c71c7550c6d
Lukas Santner <lukas@santi.gg>
2024-07-17 18:42:50 +0100

    different nvim configs for server and regular

---
+++ {server}neovim/.config/nvim/init.lua
@@ -0,0 +1,25 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.opt.clipboard = "unnamed"
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.relativenumber = true
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+require("lazy").setup({ spec = { { import = "plugins" } } })
